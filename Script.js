5f506586d0787e2872a81fe9eb23548c0a43e37c
minikazeomips-web <minikazeomips@gmail.com>
2026-01-05 12:12:08 +0800

    change

--- Script.js
+++ Script.js
@@ -1,7 +1,6 @@
 const { chromium } = require('playwright');
 
-//async IIFE to enable use of await at the top level
-(async () => {
+async function runAutomation() {    
 
 // Headless: false to see the browser action
     const browser = await chromium.launch({ headless: false });
@@ -10,7 +9,7 @@ const { chromium } = require('playwright');
 //Await promise to navigate to duckduckgo
     await page.goto('https://duckduckgo.com/');
     const searchBox = page.locator('input[name="q"]');
-    await searchBox.fill('Rocket Raccoon Marvel', delay = 100);
+    await searchBox.fill('Rocket Raccoon Marvel');
     await searchBox.press('Enter');
 // Click the first search result
     const firstResult = page.locator("#r1-0").first();
@@ -20,5 +19,6 @@ const { chromium } = require('playwright');
     await page.screenshot({ path: './Screenshots/rocket-raccoon.png', fullPage: false });
 
     await browser.close();
-})();
+}
 
+runAutomation();
